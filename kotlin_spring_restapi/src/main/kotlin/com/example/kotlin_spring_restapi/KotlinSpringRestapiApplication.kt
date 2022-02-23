a88fde8d1ff01302f3225b1a808e45f0b4534d62
muta <muttaroni8827@gmail.com>
2022-02-24 01:13:20 +0900

    feat(project): create initlal project for restapi construction with kotlin

---
+++ kotlin_spring_restapi/src/main/kotlin/com/example/kotlin_spring_restapi/KotlinSpringRestapiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_spring_restapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringRestapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringRestapiApplication>(*args)
+}
