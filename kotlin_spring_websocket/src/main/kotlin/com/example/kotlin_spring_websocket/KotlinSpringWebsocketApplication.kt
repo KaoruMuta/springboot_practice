8244f5f64b852fbcc96fdadef333f21639d0eb6b
muta <muttaroni8827@gmail.com>
2022-06-03 23:48:26 +0900

    feat(project): create initial project for websocket app construction with kotlin

---
+++ kotlin_spring_websocket/src/main/kotlin/com/example/kotlin_spring_websocket/KotlinSpringWebsocketApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_spring_websocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringWebsocketApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringWebsocketApplication>(*args)
+}
---
+++ kotlin_spring_websocket/src/test/kotlin/com/example/kotlin_spring_websocket/KotlinSpringWebsocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_spring_websocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringWebsocketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
